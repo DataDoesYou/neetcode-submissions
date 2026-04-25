53b71296800ff63ade5459e1abadf80999d39ae4
DataDoesYou <datadoesyou@gmail.com>
2026-04-25 17:38:17 -0400

    Add: python-type-conversion-input - submission-0

---
+++ Python For Beginners/python-type-conversion-input/submission-0.py
@@ -0,0 +1,16 @@
+def read_integer() -> int:
+    my_integer = int(input())
+    return my_integer
+
+def read_float() -> float:
+    my_float = float(input())
+    return my_float
+
+# do not modify below this line
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
